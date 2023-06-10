1a3c8ad0b6a7ec5c5331af6cedf94fd015dde372
manav-khadka <manavkhadka2004@gmail.com>
2023-06-11 00:49:06 +0530

    features.dart is created

---
+++ lib/home/features.dart
@@ -0,0 +1,16 @@
+import 'package:codebooter_study_app/widgets/bigText.dart';
+import 'package:flutter/material.dart';
+
+class features extends StatefulWidget {
+  const features({super.key});
+
+  @override
+  State<features> createState() => _featuresState();
+}
+
+class _featuresState extends State<features> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
