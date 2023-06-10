bf58a0e9d00233e9d00d2eb93cbccca185f1d1ec
manavkhadka <manavkhadka2004@gmail.com>
2023-06-10 12:49:18 +0530

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, world!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}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
