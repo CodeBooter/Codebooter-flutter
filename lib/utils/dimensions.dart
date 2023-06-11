d74c5244486a6c60de3f2339506adaf5eb3740d6
manav-khadka <manavkhadka2004@gmail.com>
2023-06-11 15:39:09 +0530

    dimension.dart is created

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,24 @@
+import 'package:codebooter_study_app/home/features.dart';
+import 'package:get/get.dart';
+
+import 'package:flutter/material.dart';
+
+class dimension {
+  static double get screenHeight => Get.height; //866.2857142857143
+  static double get screenWidth => Get.width; //411.428571428571444
+  static double get topPadding => Get.height / 15; //57.75238095238096
+
+  static double get val45 => Get.height / 19.25; //45
+  static double get val20 => Get.height / 43.31; // 20
+  static double get val10 => Get.height / 86.62; //10
+
+  static double get width355 => Get.width / 1.16; //355
+  static double get width180 => Get.width / 2.4; //180
+
+  static double get height132 => Get.height / 6.56; //132
+  static double get height115 => Get.height / 7.52; //115
+
+  static double get font24 => Get.height / 36.1; //24
+  static double get font16 => Get.height / 54.15; //16
+  static double get font20 => Get.height / 43.31; //20
+}
