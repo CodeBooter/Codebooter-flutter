8878e8970315da740f377ddc948d6dffe0ddf158
manav <manavkhadka2004@gmail.com>
2023-06-28 19:24:42 +0530

    admin post success

--- lib/Admin/home/PostJob.dart
+++ lib/Admin/home/PostJob.dart
@@ -1,10 +1,180 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-class PostJob extends StatelessWidget {
-  const PostJob({super.key});
+class PostJob extends StatefulWidget {
+  @override
+  _PostJobState createState() => _PostJobState();
+}
+
+class _PostJobState extends State<PostJob> {
+  final _formKey = GlobalKey<FormState>();
+
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _imageAssetController = TextEditingController();
+  TextEditingController _stipendController = TextEditingController();
+  TextEditingController _locationController = TextEditingController();
+  TextEditingController _companyController = TextEditingController();
+  TextEditingController _applyLinkController = TextEditingController();
+  TextEditingController _durationController = TextEditingController();
+  TextEditingController _applyByController = TextEditingController();
+  TextEditingController _deadlineController = TextEditingController();
+
+  Future<void> createJob() async {
+    var url =
+        'https://jobinternshipapi-production.up.railway.app/api/v1/jobs/newjob'; // Replace with your API endpoint URL
+
+    var jobData = {
+      'title': _titleController.text,
+      'imageAssest': _imageAssetController.text,
+      'stipend': _stipendController.text,
+      'location': _locationController.text,
+      'company': _companyController.text,
+      'applyLink': _applyLinkController.text,
+      'duration': _durationController.text,
+      'applyBy': _applyByController.text,
+      'deadline': _deadlineController.text,
+    };
+
+    var headers = {'Content-Type': 'application/json'};
+    var body = jsonEncode(jobData);
+
+    try {
+      var response = await http.post(
+        Uri.parse(url),
+        headers: headers,
+        body: body,
+      );
+
+      if (response.statusCode == 201) {
+        // Job created successfully
+        print('Job created successfully');
+      } else {
+        // Handle error
+        print('Error creating job: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle error
+      print('Error creating job: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Job'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  decoration: InputDecoration(labelText: 'Title'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter job title';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _imageAssetController,
+                  decoration: InputDecoration(labelText: 'Image Asset'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter image asset';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _stipendController,
+                  decoration: InputDecoration(labelText: 'Stipend'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter stipend';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _locationController,
+                  decoration: InputDecoration(labelText: 'Location'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter location';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _companyController,
+                  decoration: InputDecoration(labelText: 'Company'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter company name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _applyLinkController,
+                  decoration: InputDecoration(labelText: 'Apply Link'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter apply link';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _durationController,
+                  decoration: InputDecoration(labelText: 'Duration'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter duration';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _applyByController,
+                  decoration: InputDecoration(labelText: 'Apply By'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter apply by date';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _deadlineController,
+                  decoration: InputDecoration(labelText: 'Deadline'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter deadline';
+                    }
+                    return null;
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      createJob();
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
