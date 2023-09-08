d27592d8124a4f1c34440aeacb54d930bbd826be
Anushka-Yadav <lncbbtcse026@lnctu.ac.in>
2023-09-08 19:04:54 +0530

    Basic starting aiml full screen enabled

--- lib/Client/Screens/courses/AIML/BasicStarting.dart
+++ lib/Client/Screens/courses/AIML/BasicStarting.dart
@@ -1,11 +1,13 @@
 import 'package:codebooter_study_app/Client/Screens/courses/VideoPlayer.dart';
 import 'package:codebooter_study_app/utils/Dimensions.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart'; // Import the SystemChrome class
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import '../../../../utils/Colors.dart';
 import '../YoutubeFunction.dart';
 import 'package:provider/provider.dart';
 import 'package:codebooter_study_app/AppState.dart';
+
 class BasicStarting extends StatelessWidget {
   const BasicStarting({Key? key}) : super(key: key);
 
@@ -16,16 +18,20 @@ class BasicStarting extends StatelessWidget {
     YoutubeFunction youtubeFunction = YoutubeFunction();
     return Scaffold(
       appBar: AppBar(
-        iconTheme:  IconThemeData(color: appState.isDarkMode ? Colors.white : Colors.black,),
+        iconTheme: IconThemeData(
+          color: appState.isDarkMode ? Colors.white : Colors.black,
+        ),
         centerTitle: true,
         backgroundColor: appState.isDarkMode
             ? AppColors.primaryColor
             : AppColors.lightModePrimary,
-        title:  Text(
+        title: Text(
           'Basics',
-          style: TextStyle(color:  appState.isDarkMode
-          ? AppColors.lightModePrimary
-              : AppColors.primaryColor,),
+          style: TextStyle(
+            color: appState.isDarkMode
+                ? AppColors.lightModePrimary
+                : AppColors.primaryColor,
+          ),
         ),
       ),
       body: FutureBuilder<List<dynamic>>(
@@ -40,7 +46,7 @@ class BasicStarting extends StatelessWidget {
                 final item = playlistItems[index];
                 final title = item['snippet']['title'];
                 final thumbnailUrl =
-                    item['snippet']['thumbnails']['default']['url'];
+                item['snippet']['thumbnails']['default']['url'];
 
                 final channelId = item['snippet']['channelId'];
                 final videoId = item['snippet']['resourceId']['videoId'];
@@ -67,11 +73,15 @@ class BasicStarting extends StatelessWidget {
 
                       return InkWell(
                         onTap: () {
+                          // Disable landscape mode when entering full-screen
+                          SystemChrome.setPreferredOrientations([
+                            DeviceOrientation.portraitUp,
+                          ]);
+
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) =>
-                                  VideoScreen(videoId: videoId),
+                              builder: (context) => VideoScreen(videoId: videoId),
                             ),
                           );
                         },
@@ -98,25 +108,23 @@ class BasicStarting extends StatelessWidget {
                                 return Row(
                                   children: [
                                     Text(' $channelName'),
-                                     SizedBox(width: dimension.val10),
+                                    SizedBox(width: dimension.val10),
                                     Icon(
                                       Icons.thumb_up_alt_outlined,
                                       size: dimension.font14,
                                     ),
                                     Text(
                                       '$likeCount',
-                                      style:
-                                          TextStyle(fontSize: dimension.font14),
+                                      style: TextStyle(fontSize: dimension.font14),
                                     ),
-                                     SizedBox(width: dimension.val10),
+                                    SizedBox(width: dimension.val10),
                                     Icon(
                                       Icons.remove_red_eye_outlined,
                                       size: dimension.font14,
                                     ),
                                     Text(
                                       ' $viewCount',
-                                      style:
-                                          TextStyle(fontSize: dimension.font14),
+                                      style: TextStyle(fontSize: dimension.font14),
                                     ),
                                   ],
                                 );
@@ -130,11 +138,15 @@ class BasicStarting extends StatelessWidget {
                     } else {
                       return InkWell(
                         onTap: () {
+                          // Disable landscape mode when entering full-screen
+                          SystemChrome.setPreferredOrientations([
+                            DeviceOrientation.portraitUp,
+                          ]);
+
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) =>
-                                  VideoScreen(videoId: videoId),
+                              builder: (context) => VideoScreen(videoId: videoId),
                             ),
                           );
                         },
@@ -159,6 +171,75 @@ class BasicStarting extends StatelessWidget {
   }
 }
 
+class VideoScreen extends StatefulWidget {
+  final String videoId;
+
+  VideoScreen({required this.videoId});
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoId,
+      flags: YoutubePlayerFlags(
+        autoPlay: true,
+        // Use this option to enable full-screen when the video starts
+        isLive: true,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Customize the AppBar for full-screen video mode if needed
+        title: Text('Video Player'),
+        // Add a back button to return to portrait mode
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Re-enable landscape mode when returning to the list view
+            SystemChrome.setPreferredOrientations([
+              DeviceOrientation.portraitUp,
+              DeviceOrientation.landscapeLeft,
+              DeviceOrientation.landscapeRight,
+            ]);
+
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressIndicatorColor: Colors.blueAccent,
+          onReady: () {
+            // You can handle video ready event here
+          },
+          onEnded: (data) {
+            // You can handle video ended event here
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
 Widget buildListTile({
   required String thumbnailUrl,
   required String title,
@@ -172,7 +253,7 @@ Widget buildListTile({
     child: Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(dimension.val5),
-        border: Border.all(color: AppColors.shadowColor)
+        border: Border.all(color: AppColors.shadowColor),
       ),
       child: ListTile(
         leading: ClipRRect(
