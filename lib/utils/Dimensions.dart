fbc68f9220735cd6edd45203c4c677d0a8f6f0b8
apikmeister <afiq.mohamad90@gmail.com>
2023-10-13 23:16:28 +0800

    remove unwanted dimensions

--- lib/utils/Dimensions.dart
+++ lib/utils/Dimensions.dart
@@ -1,113 +1,75 @@
 import 'package:get/get.dart';
 
 class dimension {
+  // Screen dimensions
   static double get screenHeight => Get.height; //screen height
   static double get screenWidth => Get.width; //screen width
+
+  // Paddings & Margins
   static double get topPadding => Get.height / 15; //57.75238095238096
 
+  // Height
+  static double get val2 => Get.height / 433.125; //2
+  static double get val5 => Get.height / 173.25; //5
+  static double get val10 => Get.height / 86.62; //10
+  static double get val15 => Get.height / 57.75; //15
+  static double get val20 => Get.height / 43.31; // 20
+  static double get val25 => Get.height / 34.52; //25
+  static double get val30 => Get.height / 28.88; //30
+  static double get val34 => Get.height / 25.29; //34
+  static double get val35 => Get.height / 24.75; //35
+  static double get val40 => Get.height / 21.65; //40
+  static double get val45 => Get.height / 19.25; //45
+  static double get val50 => Get.height / 17.32; //50
   static double get val60 => Get.height / 14.44; //60
   static double get val70 => Get.height / 12.38; //70
-  static double get val50 => Get.height / 17.32; //50
-  static double get val45 => Get.height / 19.25; //45
-  static double get val40 => Get.height / 21.65; //40
-  static double get val35 => Get.height / 24.75; //35
-  static double get val34 => Get.height / 25.29; //34
-  static double get val30 => Get.height / 28.88; //30
-  static double get val25 => Get.height / 34.52; //25
 
-  static double get val20 => Get.height / 43.31; // 20
-  static double get val15 => Get.height / 57.75; //15
-  static double get val10 => Get.height / 86.62; //10
-  static double get val5 => Get.height / 173.25; //5
-  static double get val2 => Get.height / 433.125; //2
-
-  static double get width360 => Get.width / 1.11; //360
-
-  static double get width355 => Get.width / 1.16; //355
-  static double get width330 => Get.width / 1.21; //330
-  static double get width331 => Get.width / 1.21; //331
-  static double get width311 => Get.width / 1.32; //311
-  static double get width300 => Get.width / 1.36; //300
-  static double get width280 => Get.width / 1.45; //280
-  static double get width270 => Get.width / 1.49; //270
-  static double get width260 => Get.width / 1.53; //260
-  static double get width250 => Get.width / 1.58; //250
-  static double get width240 => Get.width / 1.64; //240
-
-  static double get width220 => Get.width / 1.81; //220
-  static double get width210 => Get.width / 1.9; //210
-  static double get width200 => Get.width / 2.02; //200
-  static double get width190 => Get.width / 2.14; //190
-  static double get width182 => Get.width / 2.19; //182
-  static double get width181 => Get.width / 2.2; //181
-  static double get width185 => Get.width / 2.19; //185
-  static double get width180 => Get.width / 2.4; //180
-  static double get width175 => Get.width / 2.45; //175
-  static double get width170 => Get.width / 2.5; //170
-  static double get width165 => Get.width / 2.49; //165
-  static double get width160 => Get.width / 2.56; //160
-  static double get width150 => Get.width / 2.72; //150
-  static double get width140 => Get.width / 2.96; //140
-  static double get width130 => Get.width / 3.2; //130
-  static double get width120 => Get.width / 3.52; //120
-  static double get width110 => Get.width / 3.84; //110
-  static double get width105 => Get.width / 4.19; //105
-  static double get width100 => Get.width / 4.4; //100
-  static double get width95 => Get.width / 4.72; //95
-  static double get width92 => Get.width / 4.8; //92
-  static double get width90 => Get.width / 4.88; //90
-
-  static double get width80 => Get.width / 5.5; //80
-  static double get width70 => Get.width / 6.28; //70
-  static double get width68 => Get.width / 6.44; //68
-  static double get width65 => Get.width / 6.66; //65
-  static double get width60 => Get.width / 7.33; //60
-  static double get width50 => Get.width / 8.8; //50
-  static double get width40 => Get.width / 11; //40
-  static double get width35 => Get.width / 12.57; //35
-  static double get width30 => Get.width / 14.66; //30
-  static double get width25 => Get.width / 17.6; //25
-  static double get width20 => Get.width / 22; //20
-  static double get width15 => Get.width / 29.33; //15
-  static double get width10 => Get.width / 44; //10
-
-  static double get height400 => Get.height / 2.16; //400
-  static double get height350 => Get.height / 2.47; //350
-  static double get height300 => Get.height / 2.88; //300
+  static double get height80 => Get.height / 10.8; //80
+  static double get height85 => Get.height / 10.19; //85
+  static double get height90 => Get.height / 9.64; //90
+  static double get height92 => Get.height / 9.34; //92
+  static double get height100 => Get.height / 8.66; //100
+  static double get height110 => Get.height / 7.87; //110
+  static double get height115 => Get.height / 7.52; //115
+  static double get height120 => Get.height / 7.31; //120
+  static double get height125 => Get.height / 6.96; //125
+  static double get height130 => Get.height / 6.61; //130
+  static double get height132 => Get.height / 6.56; //132
+  static double get height140 => Get.height / 6.17; //140
+  static double get height145 => Get.height / 6.03; //145
+  static double get height150 => Get.height / 5.76; //150
+  static double get height155 => Get.height / 5.65; //155
+  static double get height160 => Get.height / 5.5; //160
+  static double get height165 => Get.height / 5.3; //165
+  static double get height170 => Get.height / 5.12; //170
+  static double get height175 => Get.height / 4.97; //175
+  static double get height180 => Get.height / 4.8; //180
+  static double get height185 => Get.height / 4.64; //185
+  static double get height190 => Get.height / 4.52; //190
+  static double get height200 => Get.height / 4.32; //200
+  static double get height230 => Get.height / 3.67; // 230
   static double get height250 => Get.height / 3.46; //250
 
-  static double get height230 => Get.height / 3.67; // 230
-  static double get height200 => Get.height / 4.32; //200
-  static double get height190 => Get.height / 4.52; //190
-  static double get height185 => Get.height / 4.64; //185
-  static double get height183 => Get.height / 4.7; //183
-  static double get height180 => Get.height / 4.8; //180
-  static double get height175 => Get.height / 4.97; //175
-  static double get height170 => Get.height / 5.12; //170
-  static double get height165 => Get.height / 5.3; //165
-  static double get height160 => Get.height / 5.5; //160
-  static double get height155 => Get.height / 5.65; //155
-  static double get height153 => Get.height / 5.72; //153
-  static double get height150 => Get.height / 5.76; //150
-  static double get height145 => Get.height / 6.03; //145
-  static double get height140 => Get.height / 6.17; //140
-  static double get height135 => Get.height / 6.4; //135
-  static double get height132 => Get.height / 6.56; //132
-  static double get height130 => Get.height / 6.61; //130
-  static double get height128 => Get.height / 6.68; //128
-  static double get height125 => Get.height / 6.96; //125
-  static double get height120 => Get.height / 7.31; //120
-
-  static double get height115 => Get.height / 7.52; //115
-  static double get height113 => Get.height / 7.68; //113
-  static double get height110 => Get.height / 7.87; //110
-  static double get height105 => Get.height / 8.22; //105
-  static double get height100 => Get.height / 8.66; //100
-  static double get height95 => Get.height / 9.1; //95
-  static double get height92 => Get.height / 9.34; //92
-  static double get height90 => Get.height / 9.64; //90
-  static double get height85 => Get.height / 10.19; //85
-  static double get height80 => Get.height / 10.8; //80
+  // Width
+  static double get width10 => Get.width / 44; //10
+  static double get width15 => Get.width / 29.33; //15
+  static double get width25 => Get.width / 17.6; //25
+  static double get width30 => Get.width / 14.66; //30
+  static double get width35 => Get.width / 12.57; //35
+  static double get width50 => Get.width / 8.8; //50
+  static double get width68 => Get.width / 6.44; //68
+  static double get width100 => Get.width / 4.4; //100
+  static double get width105 => Get.width / 4.19; //105
+  static double get width110 => Get.width / 3.84; //110
+  static double get width130 => Get.width / 3.2; //130
+  static double get width140 => Get.width / 2.96; //140
+  static double get width150 => Get.width / 2.72; //150
+  static double get width165 => Get.width / 2.49; //165
+  static double get width170 => Get.width / 2.5; //170
+  static double get width180 => Get.width / 2.4; //180
+  static double get width280 => Get.width / 1.45; //280
+  static double get width311 => Get.width / 1.32; //311
+  static double get width360 => Get.width / 1.11; //360
 
   static double get font24 => Get.height / 36.1; //24
   static double get font16 => Get.height / 54.15; //16
